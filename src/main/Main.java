ae2b05964b393cddf4671a069f5f79900ee6ee47
Iqius <95892912+Iqius@users.noreply.github.com>
2024-05-31 10:24:27 +0700

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,16 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        int result1 = calculator.add(2, 3);
-        System.out.println("3 + 3 = " + result1); // Output: 2 + 3 = 5
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-        int result2 = calculator.subtract(5, 2);
-        System.out.println("5 - 2 = " + result2); // Output: 5 - 2 = 3
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 
-        int result3 = calculator.multiply(4, 3);
-        System.out.println("4 * 3 = " + result3); // Output: 4 * 3 = 12
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
-        int result4 = calculator.divide(10, 2);
-        System.out.println("10 / 2 = " + result4); // Output: 10 / 2 = 5
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Tidak dapat membagi dengan nol");
+        }
+        return a / b;
+    }
     }
 }
